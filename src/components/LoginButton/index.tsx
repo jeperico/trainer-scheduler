8269629dd2e72e067dc90014528ec3fe82141969
João Périco <joaoeperico@gmail.com>
2025-02-07 23:18:49 -0300

    [feat] clear

--- src/components/LoginButton/index.tsx
+++ src/components/LoginButton/index.tsx
@@ -1,24 +1,24 @@
-import React from "react";
-import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+// import React from "react";
+// import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 
-const clientID = process.env.NEXT_PUBLIC_CLIENT_ID;
+// const clientID = process.env.NEXT_PUBLIC_CLIENT_ID;
 
-const LoginButton = () => {
-  const onSuccess = (res: any) => {
-    console.log("Current User:", res.profileObj);
-    console.log(res);
-  };
-  const onFailure = () => {
-    console.log("Login failed:");
-  };
+// const LoginButton = () => {
+//   const onSuccess = (res: any) => {
+//     console.log("Current User:", res.profileObj);
+//     console.log(res);
+//   };
+//   const onFailure = () => {
+//     console.log("Login failed:");
+//   };
 
-  return clientID ? (
-    <GoogleOAuthProvider clientId={clientID}>
-      <GoogleLogin onSuccess={onSuccess} onError={onFailure} />
-    </GoogleOAuthProvider>
-  ) : (
-    <></>
-  );
-};
+//   return clientID ? (
+//     <GoogleOAuthProvider clientId={clientID}>
+//       <GoogleLogin onSuccess={onSuccess} onError={onFailure} />
+//     </GoogleOAuthProvider>
+//   ) : (
+//     <></>
+//   );
+// };
 
-export default LoginButton;
+// export default LoginButton;
--- src/components/LogoutButton/index.tsx
+++ src/components/LogoutButton/index.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { googleLogout } from "@react-oauth/google";
-import { Button } from "../ui/button";
+// import React from "react";
+// import { googleLogout } from "@react-oauth/google";
+// import { Button } from "../ui/button";
 
-const LogoutButton = () => {
-  const handleLogout = () => {
-    googleLogout();
-    console.log("Logout successful!");
-  };
+// const LogoutButton = () => {
+//   const handleLogout = () => {
+//     googleLogout();
+//     console.log("Logout successful!");
+//   };
 
-  return <Button onClick={handleLogout}>Logout</Button>;
-};
+//   return <Button onClick={handleLogout}>Logout</Button>;
+// };
 
-export default LogoutButton;
+// export default LogoutButton;
